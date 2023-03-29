401f84541015dab4762b26eebcda8c8c58ecd9bd
Simon McEnlly <simon.mcenlly@gmail.com>
2023-03-30 08:46:04 +1000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ziggy from './common';
+
+console.log(ziggy());
 
 function App() {
   return (
---
+++ src/common.ts
@@ -0,0 +1,3 @@
+export default function ziggy() {
+  return 'foo';
+}
